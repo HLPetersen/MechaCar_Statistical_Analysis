7b88090af570e109359bec6d88f66c87ad8f9a13
HLPetersen <116980760+HLPetersen@users.noreply.github.com>
2023-03-06 00:15:33 -0700

    Fixed capital letter in one of the variables

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,18 +2,18 @@
 library(dplyr);
 
 #Read csv as a dataframe
-Mechacar_mpg<- read.csv(file="~/Projects/MechaCar_Statistical_Analysis/MechaCar_mpg.csv", stringsAsFactors = T);
+MechaCar_mpg<- read.csv(file="~/Projects/MechaCar_Statistical_Analysis/MechaCar_mpg.csv", stringsAsFactors = T);
 
 #Perform multiple linear regression on dataframe. 
-model<-lm(MechaCar_mpg$mpg~MechaCar_mpg$vehicle_length+MechaCar_mpg$vehicle_weight+MechaCar_mpg$spoiler_angle+MechaCar_mpg$ground_clearance+MechaCar_mpg$AWD,data=MechaCar_mpg)
+model <-lm(MechaCar_mpg$mpg~MechaCar_mpg$vehicle_length+MechaCar_mpg$vehicle_weight+MechaCar_mpg$spoiler_angle+MechaCar_mpg$ground_clearance+MechaCar_mpg$AWD,data=MechaCar_mpg)
 
 #Summarize the model.
 summary(model);
 
-#Read csv as a dataframe
+#Read suspension coild csv as a dataframe
 Suspension_Coil<- read.csv(file="~/Projects/MechaCar_Statistical_Analysis/Suspension_coil.csv", stringsAsFactors = F);
 
-#Create a summary dataframe from the supsension coil csv.
+#Create a summary dataframe
 total_summary = summarize(Suspension_Coil, Mean=mean(Suspension_Coil$PSI), Median=median(Suspension_Coil$PSI), Variance = var(Suspension_Coil$PSI), SD = sd(Suspension_Coil$PSI));
 
 #Create a lot summary dataframe that organizes mean, median, variance, and standard deviation by lot number.
@@ -26,3 +26,4 @@ t.test(x=Suspension_Coil$PSI, mu=1500);
 t.test(x=subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot=='Lot1'), mu=1500);
 t.test(x=subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot=='Lot2'), mu=1500);
 t.test(x=subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot=='Lot3'), mu=1500);
+
